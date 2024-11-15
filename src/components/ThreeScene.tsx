e070b4d83df4139abd9c4022fec8162134eefc30
Chihab Jraoui <contact@chihabjraoui.com>
2024-11-15 17:38:33 +0100

    Updates

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -25,18 +25,29 @@ const ThreeScene: React.FC = () => {
       // camera.rotation.set(-50, 0, 0);
       camera.lookAt(0, 1, 0);
 
-      const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
-      groundGeometry.rotateX(-Math.PI / 2);
-      const groundMaterial = new THREE.MeshStandardMaterial({
-        color: 0x555555,
-        side: THREE.DoubleSide,
-      });
-      const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-      scene.add(groundMesh);
+      // const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+      // groundGeometry.rotateX(-Math.PI / 2);
+      // const groundMaterial = new THREE.MeshStandardMaterial({
+      //   color: 0x555555,
+      //   side: THREE.DoubleSide,
+      // });
+      // const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+      // scene.add(groundMesh);
 
-      const spotlight = new THREE.SpotLight(0xffffff, 1000, 100, 0.2, 0.5);
-      spotlight.position.set(0, 20, 0);
+      const spotlight = new THREE.SpotLight(0xffffff, 5000, 100, 0.2, 0.5);
+      spotlight.position.set(10, 30, -10);
+      spotlight.lookAt(0, 0, 0);
       scene.add(spotlight);
+
+      const spotlight2 = new THREE.SpotLight(0xffffff, 5000, 100, 0.2, 0.5);
+      spotlight2.position.set(-10, 30, -10);
+      spotlight2.lookAt(0, 0, 0);
+      scene.add(spotlight2);
+
+      const spotlight3 = new THREE.SpotLight(0xffffff, 5000, 100, 0.2, 0.5);
+      spotlight3.position.set(-0, -30, -10);
+      spotlight3.lookAt(0, 0, 0);
+      scene.add(spotlight3);
 
       // const geometry = new THREE.BoxGeometry();
       // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -47,7 +58,7 @@ const ThreeScene: React.FC = () => {
       // renderer.render(scene, camera);
 
       const loader = new GLTFLoader().setPath("/3d_models/");
-      loader.load("nissan.glb", (gltf) => {
+      loader.load("buoy.gltf", (gltf) => {
         const mesh = gltf.scene;
         mesh.position.set(0, 0, 0);
         // mesh.scale.set(10, 10, 0);
